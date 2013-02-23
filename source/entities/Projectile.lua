8084c62bbc52c4d3b35ca20c70389a81202e9506
oryanm <oryan.mat@gmail.com>
2013-02-23 13:18:00 +0200

    added projectile

---
+++ source/entities/Projectile.lua
@@ -0,0 +1,26 @@
+Projectile = class('Projectile', GameObject)
+Projectile.sequence = 0
+
+function Projectile:initialize(target, shape)
+	Projectile.sequence = Projectile.sequence + 1
+	GameObject.initialize(self, 'Projectile' .. '#' .. Projectile.sequence,
+		shape or collider:addPoint(camera._x + 100, camera._y + 300))
+	self.damage = 10
+	-- the position self wants to go to
+	self.target = target
+	self.range = PROJECTILE_RANGE
+	self.startingPosition = vector.new(self.shape:center())
+	-- a step is the product of the normalized direction vector and the projectile speed
+	-- i.e the projectile takes a step every frame in said direction and speed
+	self.step = (self.target - self.startingPosition):normalized()*PROJECTILE_SPEED
+end
+
+function Projectile:update(dt)
+	if vector.new(self.shape:center()):dist(self.startingPosition) > self.range then
+		self:destroy()
+	else
+		-- move towards the target
+		self.shape:move((self.step * dt):unpack())
+	end
+end
+
